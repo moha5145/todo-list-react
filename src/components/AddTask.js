54749eae44575fc1f86bf920c2b3cebe544ae5a7
moha5145 <mms.jimma@gmail.com>
2022-05-02 15:23:44 +0200

    add http call

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,23 +1,28 @@
-const AddTask = ({task, setTask, tasks, setTasks})=> {
-    
-    return <div className="add-task">
-        <input type="text" 
-            onChange={(event) => {
-                setTask(event.target.value)
-            }}
-        />
-        <button 
-            onClick={() => {
-                const todo= { task: task, isChecked: false}
-                setTasks((prevState) => [...prevState, todo])
-                // let clearInput = task
-                // clearInput = ''
-                // setTask("")
-                console.log(tasks)
-            }}
+import axios from 'axios'
 
+const AddTask = ({task, setTask, tasks, setTasks})=> {
+    const addTaskHandler =(event) => {
+        event.preventDefault()
+        const todo= { task: task, isChecked: false}
+        setTasks((prevState) => [...prevState, todo])
+        setTask('')
+        axios.post('http://localhost:4000/add-task', {
+                tasks
+            }).then((response) => {
+                    console.log(response)
+                })
+            }
             
-        >Add Task</button>
+            return <div className="add-task">
+        <form onSubmit={(event) => {
+            addTaskHandler(event)
+            }}>
+
+            <input type="text" value={task}
+                onChange={(event) => {setTask(event.target.value)}}
+            />
+            <button type="submit"> Add Task</button>
+        </form>
 
     </div>
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,21 +1,42 @@
+import { useEffect } from 'react'
+
+const axios = require('axios')
 const Tasks = ({tasks, setTasks})=> {
+
+    useEffect(()=> {
+            const fetchData= async () => {
+                
+                console.log('effect')
+                const response = await axios.get('http://localhost:4000/get-tasks')
+                setTasks(response.data)
+            }
+            fetchData()
+    }, [setTasks]);
+
+    const checkboxHandler = (index)=> {
+        let copiedTasks = [...tasks]
+                copiedTasks[index].isChecked = !copiedTasks[index].isChecked
+                setTasks(copiedTasks)
+    }
+
+    const deleteHandler = (index) => {
+        let copiedTasks = [...tasks]
+        copiedTasks.splice(index, 1)
+        setTasks(copiedTasks)
+    }
     return <div >
         {tasks.map((task, index)=> {
 
             return <div key={index} className="tasks">
 
-            <input type="checkbox" onClick={() => {
-
-                let copiedTasks = [...tasks]
-                copiedTasks[index].isChecked = !copiedTasks[index].isChecked
-                setTasks(copiedTasks)
+            <input type="checkbox" checked={tasks[index].isChecked} onChange={() => {
+                checkboxHandler(index)  
             }}></input>
             
             <p key="index" style={{textDecoration: task.isChecked === true ? "line-through": "none"}}>{task.task}</p>
 
             <button onClick={()=> {
-                const deleteTask = tasks.filter((task) => task !== tasks[index]);
-                setTasks(deleteTask)
+                deleteHandler(index)
             }}><i className="fa-solid fa-trash trash"></i></button>
             </div>
         })
